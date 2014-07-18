fbc7d16e6ecc85af01fabc03d7a0d873dbf2eaf1
shartep <shartep@mail.ru>
2014-07-19 00:25:57 +0300

    1907

--- HomeWork/class_String/Source.cpp
+++ HomeWork/class_String/Source.cpp
@@ -1,5 +1,17 @@
+#include "MyString.h"
 
 void main()
 {
-   ;
+   CMyString A("Hello Word!!!");
+   CMyString B( A );
+   CMyString C;
+
+
+   cout << A << endl;
+   cout << B << endl;
+   C = "PTN PTX!!!";
+   if (A==B) 
+      cout << C << endl;
+   if (A=="Hello Word!!!") 
+      cout << C << endl;
 }
