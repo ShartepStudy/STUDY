808f056fcde69a9b814cadca7e9df71f1f629108
shartep <shartep@mail.ru>
2014-10-25 17:54:54 +0300

    25-10-7

---
+++ Sokoban/button_code.h
@@ -0,0 +1,26 @@
+#ifndef BUTTON_CODE_H
+#define BUTTON_CODE_H
+
+namespace sokoban {
+
+  enum ButtonCode {
+    ENTER_BUTTON = 13,
+    ESC_BUTTON = 27,
+    SPACE_BUTTON = 32,
+    UP_BUTTON = 72,
+    LEFT_BUTTON = 75,
+    RIGHT_BUTTON = 77,
+    DOWN_BUTTON = 80,
+    ONE_BUTTON = 49,
+    TWO_BUTTON = 50,
+    THREE_BUTTON = 51,
+    R_BUTTON = 82,
+    U_BUTTON = 85,
+    r_BUTTON = 114,
+    u_BUTTON = 117,
+    EXTENDED_BUTTONS = 224
+  };
+
+} //  namespace sokoban
+
+#endif //  BUTTON_CODE_H
