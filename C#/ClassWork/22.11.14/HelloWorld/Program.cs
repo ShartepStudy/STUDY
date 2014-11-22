1487cb1dc92246bf67506993ff863c80a8cb9942
shartep <shartep@mail.ru>
2014-11-22 18:17:13 +0200

    Init

---
+++ C#/ClassWork/22.11.14/HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hellow World!");
+            Console.WriteLine(args[1]);
+            Console.WriteLine(args[0]);
+
+            int age = 30;
+            string name = "Alik";
+
+            Console.WriteLine("Привет, мое имя " + name + ". Мой возраст " + age + " лет!");
+        }
+
+    }
+}
